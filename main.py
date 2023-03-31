330644fb7d2468914af98c9bcda3d94607b97fc4
Terry <jielinl@andrew.cmu.edu>
2023-03-31 13:08:17 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from predict import *
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/students/")
+async def create_item(student: Student):
+    student_dict = student.dict()
+    result = predict(student_dict)
+    is_good = int(result['good_employee'])
+    return { 'good_employee': is_good }
